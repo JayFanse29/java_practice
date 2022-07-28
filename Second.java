03fe0cdc0af43fe65eae0447f438334e366003eb
codester-jf29 <99622128+codester-jf29@users.noreply.github.com>
2022-07-28 19:24:37 +0530

    Add files via upload

---
+++ Second.java
@@ -0,0 +1,30 @@
+public class Second
+{
+public static void main(String[] str)
+{
+
+System.out.println("Manav");
+System.out.print("Ketul");
+
+int no=101;
+String name="Dhruv";
+char gen='M';
+float height=5.6F;  //5.gf
+long sal=50000;
+double bal=50000000;
+
+int a=10,b=20;
+int c=a+b;
+System.out.print("\nADD = "+c+" A = "+a+" B = "+b);
+
+String s1="JAY",s2="FANSE";
+s1=s1+s2;
+System.out.println("\n"+s1);
+
+System.out.println(4+20+" is my number.");
+System.out.println("My number is "+4+20);
+
+int k=12;
+String s=""+k;
+System.out.print(s);
+} }
